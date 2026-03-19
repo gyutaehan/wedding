6cc07a2bdfec0eb675933e7fffadf1dcef7bcc89
gyutaehan <gyutaehan.me@gmail.com>
2026-03-19 11:05:30 +0900

    firebase키 삭제

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2NKNj_0oPPcKh2cU_TvIiJG-UXbZ6blk",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "wedding-invitation-28fc8.firebaseapp.com",
   projectId: "wedding-invitation-28fc8",
   storageBucket: "wedding-invitation-28fc8.firebasestorage.app",
